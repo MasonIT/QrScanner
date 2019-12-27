61a8869313a587eac65da5d29027f74d401a6f22
Mason <mason_it@outlook.com>
2019-12-27 11:10:13 +0800

    Turn to library

--- app/src/main/java/com/punkstudio/scanner/decode/DecodeImageCallback.kt
+++
@@ -1,8 +0,0 @@
-package com.punkstudio.scanner.decode
-
-import com.google.zxing.Result
-
-interface DecodeImageCallback {
-    fun decodeSucceed(result: Result?)
-    fun decodeFail(type: Int, reason: String?)
-}
